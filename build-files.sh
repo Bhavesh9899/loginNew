ada21d4a196248ecd3f0a8ad4e05245b4d7508bd
Bhavesh9899 <bhaveshvaghela9899@gmail.com>
2024-05-17 10:26:19 +0530

    Update build-files.sh

--- build-files.sh
+++ build-files.sh
@@ -1,5 +1,13 @@
-echo "Building the project..."
-pip install --disable-pip-version-check --target . --upgrade -r requirements.txt
+#!/bin/bash
+
+# Install required system dependencies
+apt-get update && apt-get install -y \
+    build-essential \
+    default-libmysqlclient-dev
+
+# Proceed with the usual pip install
+pip install --disable-pip-version-check --target . --upgrade -r /vercel/path0/requirements.txt
+
 
 
 echo "make migration..."
