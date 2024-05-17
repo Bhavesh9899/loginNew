3060199ec4c0810886b0832dd912cbb395462577
Bhavesh9899 <bhaveshvaghela9899@gmail.com>
2024-05-17 09:58:51 +0530

    Update build-files.sh

--- build-files.sh
+++ build-files.sh
@@ -2,8 +2,8 @@ echo "Building the project..."
 pip install -r requirements.txt
 
 echo "make migration..."
-python3.11 manage.py makemigrations --noinput
-python3.11 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 echo "collect static files..."
-python3.11 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
